25e77b361bea796e7ff0def8a733376d62d24018
417-40-noppadon <56947@yupparaj.ac.th>
2026-08-26 11:45:49 +0700

    Vat_app.py

--- Vat_app.py
+++ Vat_app.py
@@ -7,4 +7,4 @@ net_price = price - vat
 st.header(f"• ภาษีมูลค่าเพิ่ม (VAT 7%): **{vat:.2f}** บาท")
 st.header(f"• ราคาสุทธิ: {net_price:.2f} บาท")
 st.divider()
-st.write("นางสาวดีใจ ยิ้มแย้ม เลขที่ 5  ม.4/5")
+st.write("นายนพดล  จันทรา เลขที่ 40  ม.4/17")
